5894f6ad7e2726e92d0c719661d66a007a76b350
vinay <vinay@VINUTHEMONK>
2022-01-30 16:03:07 +0530

    Add project files.

---
+++ Data/BookStoreDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.API.Data
+{
+    public class BookStoreDbContext: DbContext
+    {
+        public BookStoreDbContext(DbContextOptions<BookStoreDbContext> dbContextOptions)
+            :base(dbContextOptions)
+        {
+
+
+        }
+
+        public DbSet<Books> Books { get; set; }
+    }
+}
